02fa1e62fe161b4859c7538ca15480864e61c9ba
hilly noy <hilly88@gmail.com>
2016-07-29 19:15:15 +0300

    comments

--- boto.py
+++ boto.py
@@ -7,6 +7,7 @@ import math
 import time
 from random import randint
 
+## using cookie to store the user name input
 boto_memory = {"user_name":""}
 
 @route('/', method='GET')
@@ -15,7 +16,7 @@ def index():
     return template("chatbot.html")
 
 
-
+##checking if the user typed anyword that's in our list. if so- randomizing an answer from the response list.
 def processUserMsgLevel5(msg):
     how_are_you = ['how are you', 'feel', 'feeling', 'how are you feeling', 'how you doin', 'doing']
     feel_response = ["im sooo bored today", "I feel just perfect!", "Im doing great", "I'm really tired!", "starving!!!" ]
@@ -26,12 +27,15 @@ def processUserMsgLevel5(msg):
         if any(word in how_are_you for word in user_words):
             return {"animation": "confused", "msg": feel_response[randint(0, len(feel_response)-1)]}
 
+
+##checking if the user typed any word that's in our list. if so- randomizing an answer from the response list.
 def processUserMsgLevel4(msg):
     joke_requests = ['joke', 'laugh','funny']
     robot_jokes = ['A robot walks into a bar, orders a drink, and lays down some cash. Bartender says, "Hey, we dont serve robots."And the robot says, "Oh, but someday you will."',
                    'Jack: Why was the robot angry? Ben: Beats me. Jack: Because someone kept pushing his buttons!',
                    'Billy: What did the man say to his dead robot? Bob: What? Billy: “Rust in peace.”'
 ]
+    ##eliminating '?' so that the last word can be recognized if it's in the joke request
     if msg.endswith('?'):
         msg = msg.replace(msg[len(msg) - 1], '')
     user_words = msg.split(" ")
@@ -49,10 +53,10 @@ def processUserMsgLevel3(msg):
     for word in user_words:
         if any(word in negative_list for word in user_words):
             if word in feelings_list:
-                word = word.replace(word + "?", "").replace("!", "")
                 return {"animation": "dancing", "msg": "that is fantastic! I hate being " + word}
 
 
+##checking if the user typed any word that's in our list. if so- randomizing an answer from the response list.
 def processUserMsgLevel2(msg):
 
         fact_request = ["fact","fun","facts","news","learn","teach", "new"]
@@ -76,6 +80,8 @@ def processUserMsgLevel2(msg):
                 return {"animation": "laughing", "msg": "ok..." + fun_facts[randint(0, len(fun_facts) -1)]}
         return None
 
+
+##iterating over a dictionary, if one of the user input words exsits as a key, than that key's value would be assigned to boto response
 def processUserMsgLevel1(msg):
     botoLevel1 = {
         "hi": {"animation": "inlove", "msg": "hi " + boto_memory["user_name"]},
@@ -93,7 +99,6 @@ def processUserMsgLevel1(msg):
 
     for word in user_words:
         if word in botoLevel1:
-            # word = word.replace(word+"?","").replace("!","")
             return {"animation": botoLevel1[word]["animation"], "msg": botoLevel1[word]["msg"]}
     return None
 
@@ -101,6 +106,8 @@ def processUserMsgLevel1(msg):
 @route("/chat", method='POST')
 def chat():
     user_message = request.POST.get('msg')
+
+    ##saving the first word of the first user input in the "user name" var.
     boto_memory["user_name"] = request.get_cookie("user_name")
     if not boto_memory["user_name"]:
         boto_memory["user_name"] = user_message.split(" " )[0]
@@ -108,7 +115,8 @@ def chat():
         return {"animation": "inlove", "msg": "Nice to meet you " + boto_memory["user_name"]}
 
 
-
+## looping trough 5 functions. if no user input word was found as matching the givven lists/keys than it moves over to the next function.
+## if no match was found, sending a default response to the user.
     result5 = processUserMsgLevel5(user_message)
     if result5:
         return json.dumps(result5)
